9d102ed7a02164b326df8db2d3683e9c16383f02
Adam Dinan <adam1989ie@gmail.com>
2024-11-07 10:20:37 +0000

    Revert to simpler layer size calculation

--- deep_perm/models/permeability_net.py
+++ deep_perm/models/permeability_net.py
@@ -9,23 +9,30 @@ class PermeabilityNet(nn.Module):
         super().__init__()
         self.config = config
 
-        # Build layers dynamically
-        layers = []
-        in_features = config.input_size
+        layer_sizes = [
+            config.input_size,
+            int(config.input_size * 1.2),  # First layer slightly larger than input
+            int(config.input_size * 0.8),  # Still wider than input
+            int(config.input_size * 0.4),  # Start reducing more significantly
+            int(config.input_size * 0.2),
+            int(config.input_size * 0.05),
+            int(config.input_size * 0.01),
+            2,  # Output layer
+        ]
 
-        for i, out_features in enumerate(config.hidden_sizes):
+        layers = []
+        for i in range(len(layer_sizes) - 1):
             layers.extend(
                 [
-                    nn.Linear(in_features, out_features),
-                    nn.BatchNorm1d(out_features),
+                    nn.Linear(layer_sizes[i], layer_sizes[i + 1]),
+                    nn.BatchNorm1d(layer_sizes[i + 1]),
                     nn.ReLU(),
-                    nn.Dropout(config.dropout_rates[i]),
+                    nn.Dropout(0.3),  # Original dropout rate
                 ]
             )
-            in_features = out_features
 
-        # Output layer
-        layers.extend([nn.Linear(in_features, 2), nn.BatchNorm1d(2)])
+        # Remove final ReLU and Dropout after last layer
+        layers = layers[:-2]
 
         self.layers = nn.Sequential(*layers)
         self._init_weights()
@@ -48,3 +55,51 @@ class PermeabilityNet(nn.Module):
             return F.log_softmax(x, dim=1)
         else:
             return F.softmax(x, dim=1)
+
+
+# class PermeabilityNet(nn.Module):
+#     """Neural network for permeability prediction"""
+
+#     def __init__(self, config):
+#         super().__init__()
+#         self.config = config
+
+#         # Build layers dynamically
+#         layers = []
+#         in_features = config.input_size
+
+#         for i, out_features in enumerate(config.hidden_sizes):
+#             layers.extend(
+#                 [
+#                     nn.Linear(in_features, out_features),
+#                     nn.BatchNorm1d(out_features),
+#                     nn.ReLU(),
+#                     nn.Dropout(config.dropout_rates[i]),
+#                 ]
+#             )
+#             in_features = out_features
+
+#         # Output layer
+#         layers.extend([nn.Linear(in_features, 2), nn.BatchNorm1d(2)])
+
+#         self.layers = nn.Sequential(*layers)
+#         self._init_weights()
+
+#     def _init_weights(self):
+#         for m in self.modules():
+#             if isinstance(m, nn.Linear):
+#                 nn.init.xavier_normal_(m.weight)
+#                 if m.bias is not None:
+#                     nn.init.constant_(m.bias, 0)
+
+#     def forward(self, x):
+#         """Forward pass of the neural network"""
+#         if x.dim() == 1:
+#             x = x.unsqueeze(0)
+
+#         x = self.layers(x)
+
+#         if self.training:
+#             return F.log_softmax(x, dim=1)
+#         else:
+#             return F.softmax(x, dim=1)
