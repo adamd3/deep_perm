Fix weighting check

deep_perm/trainers/trainer.py
@@ -33,7 +33,7 @@ class PermeabilityTrainer:
         self.device = device
         self.output_dir = Path(output_dir)
 
-        if use_weighted_loss and outcomes_df is not None and train_indices is not None and target_col is not None:
+        if outcomes_df is not None and train_indices is not None and target_col is not None:
             self.outcomes_df = outcomes_df.iloc[train_indices].reset_index(drop=True)
             labels = outcomes_df[target_col].values  # Use full dataset for weights
 
@@ -51,11 +51,18 @@ class PermeabilityTrainer:
             # Negative class: ~0.89 * 0.5 = 0.445
             # Positive class: ~3.68 * 0.5 = 1.84
 
-            weights = torch.tensor(weights, dtype=torch.float32).to(device)
-            self.criterion = nn.NLLLoss(weight=weights)
+            if use_weighted_loss:
+                weights = torch.tensor(weights, dtype=torch.float32).to(device)
+                self.criterion = nn.NLLLoss(weight=weights)
+            else:
+                self.criterion = nn.NLLLoss()
         else:
             self.criterion = nn.NLLLoss()
             self.outcomes_df = None
+
+        # else:
+        #     self.criterion = nn.NLLLoss()
+        #     self.outcomes_df = None
 
         #     if outcomes_df is not None and train_indices is not None and target_col is not None:
         #         self.outcomes_df = outcomes_df.iloc[train_indices].reset_index(drop=True)

deep_perm/utils/dataiq_utils.py
@@ -6,14 +6,24 @@ def classify_examples(confidence, aleatoric, dips_xthresh, dips_ythresh):
     conf_thresh_low = dips_ythresh
     conf_thresh_high = 1 - dips_ythresh
 
+    alea_perc = np.percentile(aleatoric, dips_xthresh)
+
     groups = np.empty(len(confidence), dtype=object)
 
-    groups[(confidence >= conf_thresh_high) & (aleatoric <= dips_xthresh)] = "Easy"
-    groups[(confidence <= conf_thresh_low) & (aleatoric <= dips_xthresh)] = "Hard"
+    # groups[(confidence >= conf_thresh_high) & (aleatoric <= dips_xthresh)] = "Easy"
+    # groups[(confidence <= conf_thresh_low) & (aleatoric <= dips_xthresh)] = "Hard"
+
+    # groups[
+    #     ~((confidence >= conf_thresh_high) & (aleatoric <= dips_xthresh))
+    #     & ~((confidence <= conf_thresh_low) & (aleatoric <= dips_xthresh))
+    # ] = "Ambiguous"
+
+    groups[(confidence >= conf_thresh_high) & (aleatoric <= alea_perc)] = "Easy"
+    groups[(confidence <= conf_thresh_low) & (aleatoric <= alea_perc)] = "Hard"
 
     groups[
-        ~((confidence >= conf_thresh_high) & (aleatoric <= dips_xthresh))
-        & ~((confidence <= conf_thresh_low) & (aleatoric <= dips_xthresh))
+        ~((confidence >= conf_thresh_high) & (aleatoric <= alea_perc))
+        & ~((confidence <= conf_thresh_low) & (aleatoric <= alea_perc))
     ] = "Ambiguous"
 
     return groups